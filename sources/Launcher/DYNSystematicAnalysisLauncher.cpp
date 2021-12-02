6114a03b13207e0499622570a556d868e9274f56
Le Courtois Florent <florent.lecourtois@rte-france.com>
2021-12-02 14:36:14 +0100

    #127 Fix timeline entry set

--- sources/Launcher/DYNSystematicAnalysisLauncher.cpp
+++ sources/Launcher/DYNSystematicAnalysisLauncher.cpp
@@ -96,7 +96,9 @@ SystematicAnalysisLauncher::launchScenario(const boost::shared_ptr<Scenario>& sc
 
   std::string workingDir  = createAbsolutePath(scenario->getId(), workingDirectory_);
   boost::shared_ptr<job::JobEntry> job = inputs_.cloneJobEntry();
-  job->getOutputsEntry()->getTimelineEntry()->setMaxPriority(1);
+  if (job->getOutputsEntry()->getTimelineEntry()) {
+    job->getOutputsEntry()->getTimelineEntry()->setMaxPriority(1);
+  }
   addDydFileToJob(job, scenario->getDydFile());
   setCriteriaFileForJob(job, scenario->getCriteriaFile());
 
