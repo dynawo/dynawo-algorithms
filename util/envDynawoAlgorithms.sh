d93dc0ea4963c5258eeea799bdff3ac37b6dd188
Gautier Bureau <gautier.bureau@rte-france.com>
2022-05-06 15:34:52 +0200

    #114 Add parallel on build of mpich.

--- util/envDynawoAlgorithms.sh
+++ util/envDynawoAlgorithms.sh
@@ -246,7 +246,7 @@ set_environnement() {
   export_var_env DYNAWO_GTEST_HOME=$DYNAWO_HOME
   export_var_env DYNAWO_GMOCK_HOME=$DYNAWO_HOME
 
-  export_var_env_force DYNAWO_IIDM_EXTENSION=$DYNAWO_LIBIIDM_INSTALL_DIR/lib/libdynawo_DataInterfaceIIDMExtension.so
+  export_var_env DYNAWO_IIDM_EXTENSION=$DYNAWO_LIBIIDM_INSTALL_DIR/lib/libdynawo_DataInterfaceIIDMExtension.so
   export_var_env_force DYNAWO_LIBIIDM_EXTENSIONS=$DYNAWO_LIBIIDM_INSTALL_DIR/lib
 
   # For Modelica models compilation
