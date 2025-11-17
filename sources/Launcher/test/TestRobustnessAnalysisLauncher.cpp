53417345dab8d1bdd6e46495b143b94212feed1d
Gautier Bureau <gautier.bureau@rte-france.com>
2025-11-17 12:04:31 +0100

    Fix unittest

--- sources/Launcher/test/TestRobustnessAnalysisLauncher.cpp
+++ sources/Launcher/test/TestRobustnessAnalysisLauncher.cpp
@@ -54,7 +54,7 @@ class MyLauncher : public RobustnessAnalysisLauncher {
 
     SimulationParameters params;
     result_.setScenarioId("MyScenario");
-    boost::shared_ptr<DYN::Simulation> simu = createAndInitSimulation("res", job, params, result_, inputs_);
+    boost::shared_ptr<DYN::Simulation> simu = createAndInitSimulation("res", job, params, result_, inputs_, "");
     ASSERT_TRUE(simu);
     status_t status = simulate(simu, result_);
     ASSERT_EQ(status, CONVERGENCE_STATUS);
